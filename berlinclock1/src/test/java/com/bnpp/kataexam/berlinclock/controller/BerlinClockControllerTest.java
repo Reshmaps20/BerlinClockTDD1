db75b2ca340fec97a5ba537b4a242bffa4f3dde6
reshma ps <reshmapsuresh20@gmail.com>
2024-10-10 13:23:03 +0530

    feat : No request should throw Internal Server Exception

--- berlinclock1/src/test/java/com/bnpp/kataexam/berlinclock/controller/BerlinClockControllerTest.java
+++ berlinclock1/src/test/java/com/bnpp/kataexam/berlinclock/controller/BerlinClockControllerTest.java
@@ -1,78 +1,80 @@
 package com.bnpp.kataexam.berlinclock.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
-import com.bnpp.kataexam.berlinclock.exception.TimeFormatException;
 import com.bnpp.kataexam.berlinclock.model.BerlinClockRequest;
 import com.bnpp.kataexam.berlinclock.model.BerlinClockResponse;
 import com.bnpp.kataexam.berlinclock.model.DetailedBerlinTime;
 import com.bnpp.kataexam.berlinclock.model.TimeInput;
 import com.bnpp.kataexam.berlinclock.service.BerlinClockService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static com.bnpp.kataexam.berlinclock.constants.TestConstants.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class BerlinClockControllerTest {
 
-	private TimeInput timeInput;
-	private DetailedBerlinTime detailedBerlinTime;
-	private BerlinClockRequest request;
+    @Autowired
+    MockMvc mockMvc;
+    private TimeInput timeInput;
+    private DetailedBerlinTime detailedBerlinTime;
 
-	@MockBean
-	private BerlinClockService berlinClockService;
+    private BerlinClockRequest request;
 
-	@Autowired
-	MockMvc mockMvc;
+    @BeforeEach
+    public void setup() {
+        timeInput = new TimeInput(FOURTEEN_HOUR, TWENTYFOUR_MINUTE, FIVE_SECONDS);
+        detailedBerlinTime = new DetailedBerlinTime();
+        request = new BerlinClockRequest();
+    }
 
-	@BeforeEach
-	public void setup() {
-		timeInput = new TimeInput(FOURTEEN_HOUR, TWENTYFOUR_MINUTE, FIVE_SECONDS);
-		detailedBerlinTime = new DetailedBerlinTime();
-		request = new BerlinClockRequest();
-	}
+    @Test
+    @DisplayName("Rest API to convert the time to Berlin Time")
+    public void convertTime_validRequest_shouldReturnBerlinClockResponse() throws Exception {
 
-	@Test
-	@DisplayName("Rest API to convert the time to Berlin Time")
-	public void convertTime_validRequest_shouldReturnBerlinClockResponse() throws Exception {
+        detailedBerlinTime.setSecondsLamp(OFF);
+        detailedBerlinTime.setTopFiveHourLamps(FIRST_TWO_LAMPS_RED);
+        detailedBerlinTime.setBottomOneHourLamps(ALL_FOUR_LAMPS_RED);
+        detailedBerlinTime.setTopFiveMinuteLamps(FIVE_MINT_FOURLAMPON);
+        detailedBerlinTime.setBottomOneMinuteLamps(BERLIN_TIME);
+        BerlinClockResponse expectedResponse = new BerlinClockResponse(DIGITAL_TIME, detailedBerlinTime, BERLIN_TIME);
+        request.setTime(timeInput);
 
-		detailedBerlinTime.setSecondsLamp(OFF);
-		detailedBerlinTime.setTopFiveHourLamps(FIRST_TWO_LAMPS_RED);
-		detailedBerlinTime.setBottomOneHourLamps(ALL_FOUR_LAMPS_RED);
-		detailedBerlinTime.setTopFiveMinuteLamps(FIVE_MINT_FOURLAMPON);
-		detailedBerlinTime.setBottomOneMinuteLamps(BERLIN_TIME);
-		BerlinClockResponse expectedResponse = new BerlinClockResponse(DIGITAL_TIME, detailedBerlinTime, BERLIN_TIME);
-		request.setTime(timeInput);
-		when(berlinClockService.convertToBerlinTime(any(TimeInput.class))).thenReturn(expectedResponse);
+        mockMvc.perform(post(API_PATH).contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.berlinTime").value(BERLIN_TIME));
+    }
 
-		mockMvc.perform(post(API_PATH).contentType(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(request))).andExpect(status().isOk())
-				.andExpect(jsonPath("$.berlinTime").value(BERLIN_TIME));
-	}
+    @Test
+    @DisplayName("Rest API should throw exception when invalid input is passed")
+    public void convertTime_invalidRequest_shouldReturnBadRequest() throws Exception {
 
-	@Test
-	@DisplayName("Rest API should throw exception when invalid input is passed")
-	public void convertTime_invalidRequest_shouldReturnBadRequest() throws Exception {
-		
-		timeInput = new TimeInput("", TWENTYFOUR_MINUTE, FIVE_SECONDS);
-		request.setTime(timeInput);
-		doThrow(new TimeFormatException(TIME_IS_EMPTY_ERROR)).when(berlinClockService).convertToBerlinTime(any(TimeInput.class));
-		
-		mockMvc.perform(
-				post(API_PATH).contentType(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(request)))
-				.andExpect(status().isBadRequest());
-	}
+        timeInput = new TimeInput("", TWENTYFOUR_MINUTE, FIVE_SECONDS);
+        request.setTime(timeInput);
+
+        mockMvc.perform(
+                        post(API_PATH).contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Rest API should throw internal server error when no input is passed")
+    public void convertTime_nullRequest_shouldReturnInternalServerError() throws Exception {
+
+        mockMvc.perform(
+                        post(API_PATH).contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isInternalServerError());
+    }
 }
